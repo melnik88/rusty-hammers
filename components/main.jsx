4992e4c6755bec0fd2babf81b038bd1abbdeb826
Egor Firsov <melnik88@msn.com>
2016-08-19 01:04:11 +0300

    add description

--- components/main.jsx
+++ components/main.jsx
@@ -1,73 +1,52 @@
 import React from 'react';
+import Description from './Description.jsx';
 import Workouts from './Workouts.jsx';
 import Footer from './Footer.jsx';
 
-let Main = () => (
-    <main className="wrapper">
-        <section className="container">
-            <h3>О Rusty Hammers</h3>
-            <p>Rusty Hammers - это сайт о подготовке к Гонке Героев 2017 года.</p>
+let Main = () => {
+    const tableId = 'table1';
+    return (
+        <main className="wrapper">
+            <Description tableId={tableId}/>
 
-            <h3>Построение тренировки</h3>
-            <p>В <a href="#table1">таблице 1</a> приведен список упражнений. Необходимо выбрать одно из этого списка -
-                это
-                задание на ближайшие пять дней (т.н. рабочая неделя).
-                Количество подходов и повторов в день не ограничено. Это означает, что можно выполнить все повторы в
-                один
-                день, а потом - восстанавливаться до следующего задания или же распределить нагрузку по всей неделе.
-                Такой подход учит человека самостоятельно и трезво оценивать свои силы.
-            </p>
-            <p>
-                Занятие начинается с разминки, а заканчивается заминкой. После 2-х дней тренировок обязательно следует
-                день
-                перерыва.
-            </p>
-            <p>Завершается рабочая неделя 10-15 км прогулкой.</p>
-            <p>
-                <small>1. Скоро в тренировки добавится разгрузочный день. 2. Количество упражнений будет расти. 3.
-                    Список
-                    маршрутов прогулок будет объявлен заранее
-                </small>
-            </p>
-        </section>
+            <Workouts tableId={tableId}/>
 
-        <Workouts tableId={'table1'}/>
+            <section className="container">
+                <h3>Цитата недели</h3>
+                <blockquote>
+                    <em>Ты не вечен</em>
+                    <br/>
+                    <small>
+                        <a href="https://lifehacker.ru/2016/08/15/the-most-motivational-statement/">https://lifehacker.ru/2016/08/15/the-most-motivational-statement/</a>
+                    </small>
+                </blockquote>
+            </section>
 
-        <section className="container">
-            <h3>Цитата недели</h3>
-            <blockquote>
-                <em>Ты не вечен</em>
-                <br/>
-                <small>
-                    <a href="https://lifehacker.ru/2016/08/15/the-most-motivational-statement/">https://lifehacker.ru/2016/08/15/the-most-motivational-statement/</a>
-                </small>
-            </blockquote>
-        </section>
+            <section className="container">
+                <h3>Таблица результатов</h3>
+                <h4>Егор</h4>
+                <table>
+                    <thead>
+                    <tr>
+                        <th>№</th>
+                        <th>Даты</th>
+                        <th>Упражнение</th>
+                        <th>Количество повторов</th>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>15-21.08.16</td>
+                        <td>Приседания</td>
+                        <td>500/1000</td>
+                    </tr>
+                    </thead>
+                </table>
+            </section>
 
-        <section className="container">
-            <h3>Таблица результатов</h3>
-            <h4>Егор</h4>
-            <table>
-                <thead>
-                <tr>
-                    <th>№</th>
-                    <th>Даты</th>
-                    <th>Упражнение</th>
-                    <th>Количество повторов</th>
-                </tr>
-                <tr>
-                    <td>1</td>
-                    <td>15-21.08.16</td>
-                    <td>Приседания</td>
-                    <td>500/1000</td>
-                </tr>
-                </thead>
-            </table>
-        </section>
-
-        <Footer />
-    </main>
-);
+            <Footer />
+        </main>
+    );
+};
 
 export default Main;
 
