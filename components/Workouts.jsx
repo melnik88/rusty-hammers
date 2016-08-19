fe10d514d540b1d032bae8238863a936fc932170
egor <egor.firsov@ncloudtech.ru>
2016-08-19 18:52:27 +0300

    update workouts

--- components/Workouts.jsx
+++ components/Workouts.jsx
@@ -4,65 +4,78 @@
 import React from 'react';
 
 const Workouts = ({
-    tableId
-    }) => {
-    return (
-        <section id={tableId} className="container">
-            <h3>Задание на неделю</h3>
-            <table>
-                <thead>
-                <tr>
-                    <th>Задание</th>
-                    <th>Количество повторов</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <td>Приседания</td>
-                    <td>1000</td>
-                </tr>
-                <tr>
-                    <td>Отжимания</td>
-                    <td>500</td>
-                </tr>
-                <tr>
-                    <td>Подтягивания</td>
-                    <td>250</td>
-                </tr>
-                <tr>
-                    <td>Берпи</td>
-                    <td>250</td>
-                </tr>
-                <tr>
-                    <td>Скакалка</td>
-                    <td>20 мин</td>
-                </tr>
-                <tr>
-                    <td>Пресс</td>
-                    <td>1000</td>
-                </tr>
-                <tr>
-                    <td>Прыжки на бокс</td>
-                    <td>250</td>
-                </tr>
-                <tr>
-                    <td>Отжимания на брусьях</td>
-                    <td>250</td>
-                </tr>
-                <tr>
-                    <td>Планка</td>
-                    <td>20 мин</td>
-                </tr>
-                <tr>
-                    <td>Подъем по эскалатор пешком</td>
-                    <td>20 лестниц</td>
-                </tr>
-                </tbody>
-            </table>
-            <p className="float-right">
-                <small>Таблица 1 Упражнение на одну рабочую неделю</small>
-            </p>
-        </section>)
+	tableId
+	}) => {
+	let workouts = [
+		{
+			name: 'Приседания',
+			repeats: 1000,
+			description: 'Поставь ноги на ширину плеч. Начни движение вниз, сгибая колени и опуская бедра. Опустись как можно ниже. После того, как колени согнутся чуть ниже, чем на 90 градусов, быстро возвращайся в исходное положение. Держи колени чуть согнутыми, не замыкай суставы. Держи спину и голову прямо на протяжении всего упражнения. Можно смотреть выше линии горизонта. Не отрывай пятки от земли. <a href="http://giphy.com/gifs/mens-health-bodyweight-squat-3o6Ei2vzT1Pl41e28E">Пример упражнения</a>'
+		}, {
+			name: 'Отжимания',
+			repeats: 500,
+			description: ''
+		}, {
+			name: 'Подтягивания',
+			repeats: 250,
+			description: ''
+		}, {
+			name: 'Берпи',
+			repeats: 250,
+			description: ''
+		}, {
+			name: 'Скакалка',
+			repeats: '20 мин',
+			description: ''
+		}, {
+			name: 'Пресс',
+			repeats: 1000,
+			description: ''
+		}, {
+			name: 'Прыжки на бокс',
+			repeats: 250,
+			description: ''
+		}, {
+			name: 'Отжимания на брусьях',
+			repeats: 250,
+			description: ''
+		}, {
+			name: 'Планка',
+			repeats: '20 мин',
+			description: ''
+		}, {
+			name: 'Подъем по эскалатор пешком',
+			repeats: '20 лестниц',
+			description: ''
+		}
+	];
+
+	return (
+		<section id={tableId} className="container">
+			<h3>Задание на неделю</h3>
+			<table>
+				<thead>
+				<tr>
+					<th>Задание</th>
+					<th>Количество повторов</th>
+					<th>Описание</th>
+				</tr>
+				{
+					workouts.map(() => {
+						return (
+							<tr></tr>
+						)
+					}
+				)
+				}
+				</thead>
+				<tbody>
+				</tbody>
+			</table>
+			<p className="float-right">
+				<small>Таблица 1 Упражнение на одну рабочую неделю</small>
+			</p>
+		</section>)
 };
 
 export default Workouts;
